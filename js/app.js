bdecbda0983f69c1e3f44e577081028d9e0c3da4
issam armas <almorchid123@gmail.com>
2018-11-01 16:24:52 +0000

    add smooth scrool script and delete the old

--- js/app.js
+++ js/app.js
@@ -1,7 +1,3 @@
 
 
-function changeColor() {
-
-  getElementsByClassName('paragraph').onclick=()
-  
-}
+$(function(){$("body a").click(function(b){b.preventDefault();var a=$($(this).attr("href")).offset().top;$("html,body").stop().animate({scrollTop:a-30},800)})});
