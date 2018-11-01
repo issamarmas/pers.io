27698bd79615504a6b1f19a4d437673a1e173acf
issam armas <almorchid123@gmail.com>
2018-11-01 16:42:27 +0000

    fix li contact

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
 
 
-$(function(){$("body a").click(function(b){b.preventDefault();var a=$($(this).attr("href")).offset().top;$("html,body").stop().animate({scrollTop:a-30},800)})});
+$(function(){
+  $("body a").click(function(b){
+  b.preventDefault();
+  var a=$($(this).attr("href")).offset().top;
+  $("html,body").stop().animate({scrollTop:a-30},800)})});
