2543962d912a2c1dc3f89574ecaa961398aa085e
issam armas <almorchid123@gmail.com>
2018-11-06 15:15:46 +0000

    add jQuery scripts for responsive navbar

--- js/app.js
+++ js/app.js
@@ -4,4 +4,29 @@ $(function(){
   $("body a").click(function(b){
   b.preventDefault();
   var a=$($(this).attr("href")).offset().top;
-  $("html,body").stop().animate({scrollTop:a-30},800)})});
+  $("html,body").stop().animate({scrollTop:a-30},750)})});
+
+
+
+
+      // Scrolling Effect
+
+      $(window).on("scroll", function() {
+            if($(window).scrollTop()) {
+                  $('.bg-light').removeClass('nnn');
+                  $('.bg-light >').removeClass('n');
+                  $('li >').removeClass('n');
+                  $('.s-nav').removeClass('nn');
+                  $('i').removeClass('n');
+                  $('.logo').removeClass('n');
+            }
+
+            else {
+                  $('.bg-light').addClass('nnn');
+                  $('.bg-light >').addClass('n');
+                  $('li >').addClass('n');
+                  $('.s-nav').addClass('nn');
+                  $('i').addClass('n');
+                  $('.logo').addClass('n');
+            }
+      })
